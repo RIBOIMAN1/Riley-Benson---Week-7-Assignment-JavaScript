3172ac8e4c6410a4e4d7c27079ad543bb5c9aee9
RIBOIMAN1 <rileybenson@my.whccd.edu>
2024-11-27 21:47:04 -0800

    script.js: - Gave an answer to the first asked question inside of the given console log. - Added comments to each console log in advance.

--- script.js
+++ script.js
@@ -4,19 +4,19 @@
 console.log(
   "What worked well for you over the last 9 weeks that aided in your learning process?"
 );
-console.log("");
+console.log("For me, something that worked well during these last several weeks in this course were being able to work from home and use my own personal setup. It is quite a bit easier to work on my own stuff from home, and just come to class to ask questions when necessary. I feel that my time management is a bit better because of this, and I can also work with a nicer setup from home, which also makes coding easier."); // Console log explaining the asked question and outputting the answer to the console
 
 // 2. Write a console.log() statement that answers the following question: What would you
 // change about the course?
 
 console.log("What would you change about the course?");
-console.log("");
+console.log(""); // Console log explaining the asked question and outputting the answer to the console
 
 // 3. Write a console.log() statement that answers the following question: Did you learn
 // what you expected to learn in this course?
 
 console.log("Did you learn what you expected to learn in this course?");
-console.log("");
+console.log(""); // Console log explaining the asked question and outputting the answer to the console
 
 // 4. Write a console.log() statement that answers the following question: What concepts
 // were new to you and what concepts had you encountered already in the past?
@@ -24,4 +24,4 @@ console.log("");
 console.log(
   "What concepts were new to you and what concepts had you encountered already in the past?"
 );
-console.log("");
+console.log(""); // Console log explaining the asked question and outputting the answer to the console
