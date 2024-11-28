ca341f654f333caa3bcd10a53e5984fb0eba24d9
RIBOIMAN1 <rileybenson@my.whccd.edu>
2024-11-27 22:20:45 -0800

    script.js: - Filled the remaining empty console log commands with answers to the remaining unanswered questions (2, 3, and 4).

--- script.js
+++ script.js
@@ -10,13 +10,13 @@ console.log("For me, something that worked well during these last several weeks 
 // change about the course?
 
 console.log("What would you change about the course?");
-console.log(""); // Console log explaining the asked question and outputting the answer to the console
+console.log("I would personally reduce the amount of homework that this course has, as I don't really like homework, and I am working on this class all night. It is understandable if this is impossible to address though."); // Console log explaining the asked question and outputting the answer to the console
 
 // 3. Write a console.log() statement that answers the following question: Did you learn
 // what you expected to learn in this course?
 
 console.log("Did you learn what you expected to learn in this course?");
-console.log(""); // Console log explaining the asked question and outputting the answer to the console
+console.log("I feel like I learned what I expected coming into this course. I enjoyed the linking of commands together and seeing how they work together to create many different things."); // Console log explaining the asked question and outputting the answer to the console
 
 // 4. Write a console.log() statement that answers the following question: What concepts
 // were new to you and what concepts had you encountered already in the past?
@@ -24,4 +24,4 @@ console.log(""); // Console log explaining the asked question and outputting the
 console.log(
   "What concepts were new to you and what concepts had you encountered already in the past?"
 );
-console.log(""); // Console log explaining the asked question and outputting the answer to the console
+console.log("I have been slightly familiar with the \"if\" and the \"else\" commands thanks to having done a little bit of Python in high school, on top of the \"console.log\" command being similar to Python's \"print\" command. Other than that, JavaScript has pretty much been a new experience for me."); // Console log explaining the asked question and outputting the answer to the console
